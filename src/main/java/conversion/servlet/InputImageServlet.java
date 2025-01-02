7a74b25838adb4518302942271771051407738c8
tsuzaki <t.tzk1250@gmail.com>
2025-01-03 02:25:47 +0900

    最初の内容

---
+++ src/main/java/conversion/servlet/InputImageServlet.java
@@ -0,0 +1,77 @@
+package conversion.servlet;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.opencv.core.Mat;
+
+
+/**
+ * Servlet implementation class InputImageServlet
+ */
+@WebServlet(name = "inputImage", urlPatterns = { "/inputImage" })
+public class InputImageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final String UPLOAD_DIR = "uploads";
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InputImageServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		// アップロードディレクトリのパスを取得
+        String uploadPath = getServletContext().getRealPath("") + File.separator + UPLOAD_DIR;
+        
+        // アップロードディレクトリが存在しない場合は作成
+        File uploadDir = new File(uploadPath);
+        if (!uploadDir.exists()) {
+            uploadDir.mkdir();
+        }
+        
+		try {
+			// ファイルを保存
+            String fileName = Paths.get(request.getPart("file").getSubmittedFileName()).getFileName().toString();
+            File file = new File(uploadPath, fileName);
+            request.getPart("file").write(file.getAbsolutePath());
+            
+            // 画像のURLを作成
+            String fileUrl = request.getContextPath() + "/" + UPLOAD_DIR + "/" + fileName;
+            
+            RequestDispatcher rd = request.getRequestDispatcher("/Menu.jsp");
+    		request.setAttribute("", fileUrl);
+    		rd.forward(request, response);
+		} catch (Exception e) {
+			RequestDispatcher rd = request.getRequestDispatcher("/exceptionMessage.jsp");
+			request.setAttribute("exception", e);
+			rd.forward(request, response);
+			return;
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/Menu.jsp
@@ -0,0 +1,34 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>画像変換アプリ</title>
+<link rel="stylesheet" type="text/css" href="design.css">
+</head>
+<body>
+<h1>画像変換アプリ</h1>
+変換したい画像を選んでください．
+	<div class="drop-area">
+		<p>ドラッグ&amp;ドロップしてください</p>
+		<form action="inputImage" method="post" enctype="multipart/form-data">
+			<div id="input-image">
+				<label for="file-input" style="cursor: pointer;">
+				ここに画像をドラッグ＆ドロップしてください
+				</label>
+				<input type="file" name="file" id="file-input" accept="image/*" style="display: none;" onchange="this.form.submit()">
+			</div>
+			<button type="submit">変換</button>
+		</form>
+	</div>
+	<div class="preview-area" id="preview">
+		<p>画像表示</p>
+		<div>
+			<!-- サーブレットからリダイレクトされた画像URLを取得 -->
+			<img alt="変換画像" src='<c:out value="${outimgUrl}" />'>
+		</div>
+	</div>
+</body>
+</html>
